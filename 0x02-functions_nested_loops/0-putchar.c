718b44d3069babdaa12f19059fa316970322b241
Shugo52 <olofuchubi@gmail.com>
2022-09-13 12:49:23 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-	_putchar('H');
+	_putchar('_');
+	_purchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
