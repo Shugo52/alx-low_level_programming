beeb3792e31d9c504eb26d86483062bfff05a05f
Shugo52 <olofuchubi@gmail.com>
2022-09-19 15:53:06 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters
+ *
+ * @str: character
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i += 2)
+	{
+		printf("%c,", str[i]);
+	}
+	printf("\n");
+}
