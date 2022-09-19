a16bc88a5c605e1f8e349b192290a14d5884c43f
Shugo52 <olofuchubi@gmail.com>
2022-09-19 13:30:27 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,4 +12,16 @@ void swap_int(int *a, int *b);
  * int _strlen - returns strlen
  */
 int _strlen(char *s);
+/**
+ * _putchar - prints character
+ *
+ * @c: chahracter
+ *
+ * Return: 1 or 0;
+ */
+int _putchar(char c);
+/**
+ * _puts - prints characters
+ */
+void _puts(char *str);
 #endif
