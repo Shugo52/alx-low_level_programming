86f5e7fda29b7dba6a07bae8b8422195c8628a71
Shugo52 <olofuchubi@gmail.com>
2022-09-28 10:12:02 -0700

    correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,8 @@ int _strlen_recursion(char *s)
 {
 	int counter;
 
-	if (*s != '\0')
-	{
-		counter = 1;
-		counter += _strlen_recursion(s + 1);
-	}
-	return (counter);
+	counter = 1;
+	if (*s == '\0')
+		return (0);
+	return (counter + _strlen_recursion(s + 1));
 }
