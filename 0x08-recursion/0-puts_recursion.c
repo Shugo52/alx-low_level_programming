171cc8a511ab8c98bd34c3fbf4071e31edcd80bf
Shugo52 <olofuchubi@gmail.com>
2022-09-28 07:23:00 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - recursive function
+ *
+ * @s: character
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] == '\0')
+		return;
+	_putchar(s[i]);
+	_puts_recursion(s[i + 1]);
+}
