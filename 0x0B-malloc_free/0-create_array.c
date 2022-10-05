a1e1d4e629b9ed2d1a16e0ce14482042bd498cda
Shugo52 <olofuchubi@gmail.com>
2022-10-04 17:05:06 -0700

    test

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	arr = malloc(sizeof(char) * size);
-	arr[0] = c;
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 	return (arr);
+	free(arr);
 }
