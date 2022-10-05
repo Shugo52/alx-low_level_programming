011432f7e8dafa5936bb16540245869e8314a59e
Shugo52 <olofuchubi@gmail.com>
2022-10-04 18:21:04 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,30 +20,17 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (s1 == NULL && s2 == NULL)
+		return (new_string = "");
 	j = 0;
 	n = strlen(s1) + strlen(s2) + 1;
 	new_str = malloc(n);
 	if (new_str == NULL)
 		return (NULL);
-	for (i = 0; i < n; i++)
-	{
-		if (j < strlen(s1) || j < strlen(s2))
-		{
-			if (i < strlen(s1))
-			{
-				new_str[i] = s1[j];
-			}
-			if (i == strlen(s1))
-			{
-				j = 0;
-			}
-			if (i > strlen(s1))
-			{
-				new_str[i] = s2[j];
-			}
-		}
-		j++;
-	}
+	for (i = 0; i < strlen(s1); i++)
+		new_str[i] = s1[i];
+	for (i = 0; i <= strlen(s2); i++)
+		new_str[i + strlen(s1)] = s2[i];
 	return (new_str);
 	free(new_str);
 }
