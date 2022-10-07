b043980d52573d646d146dd86c5deb0578b0ac1d
Shugo52 <olofuchubi@gmail.com>
2022-10-06 17:13:26 -0700

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * malloc_checked - checks allocated memory
+ *
+ * @b: integer to allocate memory for
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *t;
+
+	t = malloc(sizeof(unsigned int));
+	if (t == NULL)
+		exit (98);
+	return (t);
+	free (t);
+}
