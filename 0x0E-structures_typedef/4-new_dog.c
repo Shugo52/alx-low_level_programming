1671173e24b2ddc291e51a66328543d17e920c4d
Shugo52 <olofuchubi@gmail.com>
2022-10-10 18:49:56 -0700

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
+	char *dog_name;
+	char *dog_owner;
 
+	if ((n_dog->name == NULL) && (n_dog->owner == NULL))
+		return (NULL);
+
+	dog_name = malloc(strlen(name) + 1);
+	dog_owner = malloc(strlen(owner) + 1);
 	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 		return (NULL);
+
 	n_dog->name = name;
 	n_dog->age = age;
 	n_dog->owner = owner;
-	if ((n_dog->name == NULL) && (n_dog->owner == NULL))
-		return (NULL); 
 	return (n_dog);
 }
