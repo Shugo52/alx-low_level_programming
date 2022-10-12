9cfebe8030c9f80a252e35d249a8351073cd002c
Shugo52 <olofuchubi@gmail.com>
2022-10-12 11:00:50 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - function
+ *
+ * @array: array of numbers
+ * @size: size of array
+ * @cmp: array element
+ * Return: always
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
