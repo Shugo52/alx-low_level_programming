e786c336a31784718dc598b3f2ce670581b1ffef
Shugo52 <olofuchubi@gmail.com>
2022-10-13 10:50:41 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings passed as arguments
+ *
+ * @separator: separates strings passed
+ * @n: number of arguments passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	/*variables needed*/
+	size_t i;
+	va_list ap;
+
+	i = 0;
+	/*process*/
+	va_start(ap, n)
+	if (n)
+	{
+		for (1 = 0; i < n - 1; i++)
+		{
+			/*if separator is NULL, print without separator*/
+			if (separator == NULL)
+				printf("%s", va_arg(ap, char *));
+			else /*print with separator*/
+				printf("%s%s", va_arg(ap, char *), separator);
+		}
+		printf("%s", va_arg(ap, char *);
+	}
+	va_end(ap);
+	printf("\n");
+}
