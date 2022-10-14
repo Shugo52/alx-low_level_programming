1674d10b109e20421d1940f1d44f16d76e02e14c
Shugo52 <olofuchubi@gmail.com>
2022-10-13 20:53:43 -0700

    check

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,7 +10,7 @@ void print_all(const char * const format, ...)
 	char *tmp_string;
 
 	i = 0;
-	n = strlen(format - 1);
+	n = strlen(format);
 	va_start(ap, format);
 	while (i < n - 1)
 	{
@@ -39,5 +39,30 @@ void print_all(const char * const format, ...)
 		}
 		i++;
 	}
+	switch(format[i])
+                {
+                        case 'c':
+                        printf("%c", va_arg(ap, int));
+                        break;
+                        case 'i':
+                        printf("%d", va_arg(ap, int));
+                        break;
+                        case 'f':
+                        printf("%f", va_arg(ap, double));
+                        break;
+                        case 's':
+                        tmp_string = va_arg(ap, char *);
+                        if (tmp_string == NULL)
+                        {
+                                printf("(nil)");
+                                break;
+                        }
+                        printf("%s", tmp_string);
+                        break;
+                        default:
+                        break;
+                }
+
 	va_end(ap);
+	printf("\n");
 }
