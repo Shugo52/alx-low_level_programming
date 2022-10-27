a9fdc38a75ea0ace0fe5285574ce8a134b5928a3
Shugo52 <olofuchubi@gmail.com>
2022-10-27 13:36:50 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit value of binary number
+ *
+ * @n: interger to convert to binary
+ * @index: index of bit value
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int tmp[64];
+	int i, j;
+	int temp[64];
+
+	if (n == 0)
+	{
+		tmp[0] = 0;
+	}
+
+	if (n)
+	{
+		for (i = 0 ; n > 0; i++)
+		{
+			tmp[i] = n % 2;
+			n /= 2;
+		}
+
+		for (j = i - 1; j >= 0; j--)
+		{
+			temp[j] = tmp[j];
+		}
+	}
+	return (temp[index]);
+}
