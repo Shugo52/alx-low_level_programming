a38b8fe6a9371681176d1440ea208b4051be71d7
Shugo52 <olofuchubi@gmail.com>
2022-10-31 17:13:39 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to be created
+ * @text_content: content of file created
+ * Return: 1-success -1-fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, size;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		size = write(fildes, text_content, strlen(text_content));
+		if (size == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
