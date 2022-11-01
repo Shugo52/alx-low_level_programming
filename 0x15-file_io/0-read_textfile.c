ebe542560be596aa480fe869fd665295f45497b7
Shugo52 <olofuchubi@gmail.com>
2022-11-01 00:47:56 -0700

    updated file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,24 +10,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd1, size, i;
+	int fd1, size;
 	char *buffer;
 
-	buffer = malloc(sizeof(char) * letters);
-	i = 0;
+	if (!filename)
+		return (0);
+
+	buffer = malloc(letters + 1);
 	fd1 = open(filename, O_RDONLY);
 
-	if (fd1 == -1 || !(filename))
+	if (fd1 == -1)
 		return (0);
 
 	size = read(fd1, buffer, letters);
 	if (size == -1)
 		return (0);
+
+	dprintf(STDOUT_FILENO, "%s", buffer);
 	close(fd1);
-	while (buffer[i] != '\0')
-	{
-		_putchar(buffer[i]);
-		i++;
-	}
+
 	return (size);
+	free(buffer);
 }
