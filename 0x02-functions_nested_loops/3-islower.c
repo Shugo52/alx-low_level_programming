55cc61f985ebe63eedeac982359b9246c902e93f
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2022-11-26 14:51:50 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,6 @@
  * _islower - main function
  *
  * @c: character to compare
- *
  * Return: 1 if true, 0 if false
  *
  **/
@@ -11,8 +10,6 @@
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
 	return (0);
 }
