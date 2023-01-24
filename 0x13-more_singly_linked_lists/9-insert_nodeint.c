08f1d7c8d17e2fcedab60928464a127a91b45cec
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-24 13:48:46 +0100

    Task 9 - update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,74 +1,42 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a listint_t node in a listint_t list.
- *
- * @head: Double pointer to the list's beginning.
- * @idx: Index at where the new node should be inserted.
- * @n: Value for data field of new node.
- *
- * Return: Address of newly inserted node or NULL on failure.
- */
+ * insert_nodeint_at_index - inserts a node at a position in a linked list
+ * @head: pointer to the first node of the linked list
+ * @idx: position to insert node at
+ * @n: data to add in the node
+ * Return: address of new node else NULL if fail
+*/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
+	listint_t *tmp, *prev, *new_node = malloc(sizeof(listint_t));
+	size_t i = 0;
 
-	if (idx >= listint_len(*head) || (int) idx < 0)
+	if (!new_node)
 		return (NULL);
 
-	if (idx != 0)
-		return (insert_intnode_at_index((*head)->next, *head, --idx, n));
-
-	node = malloc(sizeof(listint_t));
-	if (!node)
-		return (NULL);
-
-	node->n = n;
-	node->next = *head;
-	*head = node;
-
-	return (node);
-}
-
-/**
- * listint_len - Calculates the number of nodes in a linked list.
- * @h: Linked list's head.
- *
- * Return: Number of nodes.
- */
-size_t listint_len(const listint_t *h)
-{
-	if (!h)
-		return (0);
-
-	return (1 + listint_len(h->next));
-}
-
-/**
- * insert_intnode_at_index - Inserts a listint_t node in a listint_t list.
- *
- * @head: Double Pointer to the list's beginning.
- * @prev: Previous node preceding current indexed node.
- * @idx: Index at where the new node should be inserted.
- * @n: Value for data field of new node.
- *
- * Return: Node's address or NULL on failure.
- */
-listint_t *insert_intnode_at_index(listint_t *head, listint_t *prev,
-size_t idx, int n)
-{
-	listint_t *node;
-
-	if (idx != 0)
-		return (insert_intnode_at_index(head->next, head, --idx, n));
-
-	node = malloc(sizeof(listint_t));
-	if (!node)
-		return (NULL);
-
-	node->n = n;
-	node->next = head;
-	prev->next = node;
-
-	return (node);
+	new_node->n = n;
+	tmp = *head;
+	prev = *head;
+	if (*head)
+	{
+		for (i = 0; i < idx; i++)
+		{
+			tmp = tmp->next;
+			if (!tmp)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		}
+		for (i = 0; i < idx - 1; i++)
+		{
+			prev = prev->next;
+		}
+		prev->next = new_node;
+		new_node->next = tmp;
+		return (new_node);
+	}
+	return (NULL);
 }
