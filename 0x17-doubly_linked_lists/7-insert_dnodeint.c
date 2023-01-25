1173df35e0b61aacc7f030fb3d160de19974c5a7
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-01-25 19:34:24 +0100

    Task 7 - update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,14 +37,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->next = tmp;
 		tmp->prev = new_node;
 		return (new_node);
-	} else
-	{
-		return (NULL);
 	}
-	new_node->prev = tmp->prev;
-	tmp->prev->next = new_node;
-	new_node->next = tmp;
-	tmp->prev = new_node;
-
-	return (new_node);
+	return (NULL);
 }
