50f0471dac990f4110820425b5cdda1448afe1f0
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-06-12 17:51:38 +0100

    Update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = malloc(sizeof(hash_node_t));
 	if (!key || !ht || !node || !value)
+	{
+		free(node);
 		return (0);
+	}
 
 	/*compute the index*/
 	index = key_index((unsigned char *)key, ht->size);
